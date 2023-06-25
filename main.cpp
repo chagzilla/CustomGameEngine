a6846bc7989f135f60cadfe39625ea5564c1180e
chagzilla <wjchagula@gmail.com>
2023-06-25 19:16:49 -0400

    removing unused code

--- main.cpp
+++ main.cpp
@@ -6,22 +6,6 @@
 int main() {
     Engine game;
     std::vector<std::pair<GLenum, const char *> > sData;
-	// sData.push_back(std::make_pair(GL_VERTEX_SHADER, "/Users/wilbertchagula/Documents/CustomGameEngine/shaders/offsetVShader.glsl"));
-	// // sData.push_back(std::make_pair(GL_TESS_CONTROL_SHADER, "/Users/wilbertchagula/Documents/CustomGameEngine/shaders/tcsShader.glsl"));
-	// // sData.push_back(std::make_pair(GL_TESS_EVALUATION_SHADER, "/Users/wilbertchagula/Documents/CustomGameEngine/shaders/tesShader.glsl"));
-	// sData.push_back(std::make_pair(GL_FRAGMENT_SHADER, "/Users/wilbertchagula/Documents/CustomGameEngine/shaders/fragShader.glsl"));
-	// // sData.push_back(std::make_pair(GL_GEOMETRY_SHADER, "/Users/wilbertchagula/Documents/CustomGameEngine/shaders/geoShader.glsl"));
-	// ShaderProgram p(sData);
-	// p.buildProgram();
-    // game.addShaderProg(&p);
-    // GLuint vao;
-    // std::cout << "creating vertex arrays" << std::endl;
-    // glGenVertexArrays(1, &vao);
-    // std::cout << "vertex array created" << std::endl;
-    // glBindVertexArray(vao);
-
     int status = game.run();
-    // glDeleteVertexArrays(1, &vao);
-    // glDeleteProgram(p.getProgram());
     return status;
 }
